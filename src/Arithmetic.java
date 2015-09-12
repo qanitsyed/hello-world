82043aea45bb6248bd2002ce8752cc369eb7b9c8
Qanit Al-Syed <qanit.al@gmail.com>
2015-09-12 19:09:49 -0400

    adding inital files

---
+++ src/Arithmetic.java
@@ -0,0 +1,13 @@
+
+public class Arithmetic {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = sum(3, 2);
+		System.out.println(result);
+
+	}
+	static int sum (int a, int b) {
+		return a + b;
+	}
+	
+}
